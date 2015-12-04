2fb7318c160fa5045e43b1865dbbde9387beab72
Stefano Bodini <sbodini@att.net>
2015-12-04 16:36:44 -0600

    Change heavily management to add presets and other improvements

--- UVBoxTimer/UVBoxTimer.ino
+++ UVBoxTimer/UVBoxTimer.ino
@@ -22,8 +22,9 @@ void settingManagement();
 void displaySetting();
 void secondsToTime(int, short *, short *, short * );
 void displayTime(short, short, short);
-void displayMenu(unsigned char, short);
+void displayMenu();
 int timeToSeconds(int , int , int );
+unsigned char waitingResetTimer(unsigned char);
 
 /* 
  *  I2C display defines
@@ -49,6 +50,10 @@ LiquidCrystal_I2C  lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin
 #define  ON  1
 
 #define DISPLAY_TIME	/* Comment this out in order to display seconds countdwon */
+
+#define MAX_PRESET	10  /* Max number of preset values handled */
+
+#define BURNS_ENCODER    /* If defined the encoder used is the burns one (from Adafruit) - if not defined is the one from All Electronics */
 
 /* Main state machine states defines */
 
@@ -123,16 +128,19 @@ short second = 0;
 char buffer[20];
 
 unsigned char saving_flag = 0;		        /* 0 - don't save - 1 - save */
-unsigned char toggle_flag = 0;
+unsigned char toggle_LED = 0;
 unsigned char main_status = IDLE;
-unsigned char set_status  = LOAD_PRESETS;
-unsigned char force_display_setting = 0;
+unsigned char set_status  = READ_PRESETS;
+unsigned char force_display_setting = OFF;
+unsigned char force_display_title = OFF;         /* 1 display the title */
+unsigned char preset_index = 0;
 
 const char time_menu[]   = "Time            ";
 const char logo_menu[]   = "UVBox Timer 1.0 ";
 const char preset_menu[] = "Preset #        ";
 const char select_menu[] = "Set             ";
-const char run_menu[]    = "                ";
+const char run_menu[]    = "Action:         ";
+
 
 /**
  *  @fn doEncoder
@@ -181,7 +189,7 @@ void setup()
   pinMode(BUZZER,OUTPUT);
 
   lcd.setBacklightPin(BACKLIGHT_PIN, POSITIVE); // POSITIVE); //NEGATIVE);
-  lcd.setBacklight(LED_ON);
+  lcd.setBacklight(ON);
   lcd.backlight();
     
   lcd.begin(16,2);               // initialize the lcd 
@@ -194,9 +202,16 @@ void setup()
   lcd.print (logo_menu);      
   
   displayTime(hour, minute, second);        
-  
-//  attachInterrupt(ENC_A,doEncoder,CHANGE);
+
+   /* 
+    *  The BURNS encoder needs generates a rising signal for each dent, the AllElectronic encoder generates a change signal
+    *  for each dent
+    */
+#if defined(BURNS_ENCODER)
   attachInterrupt(ENC_A,doEncoder,RISING);
+#else
+  attachInterrupt(ENC_A,doEncoder,CHANGE);
+#endif  
 }
 
 
@@ -208,18 +223,25 @@ void loop()
    // put your main code here, to run repeatedly:
    // setup loop
 
+   /*
+    *  Main state machine
+    *  Note that the pushbutton is active on the RELEASE not when pushed.
+    */
    switch(main_status)
    {
       case IDLE:
          settingManagement();		/* Handle settings */
 		 
          if(KEYIsPressed(STARTBTN) && !(hour==0 && minute == 0 && second == 0))
+         {
+            while(KEYIsPressed(STARTBTN));  /* Wait until the button is released */
+            
             main_status = START;
+            force_display_title = ON;
+         }
          break;
          
       case START:   
-         while(KEYIsPressed(STARTBTN));  /* Wait until the button is released */
-
          // timer loop
          TimerTimeSec=millis(); // timer value to actual time to display timer and decrement seconds
          TimerSecCounter=timeToSeconds(hour, minute,second)+1; // add one to account for first update
@@ -227,8 +249,6 @@ void loop()
          digitalWrite(RELAY_LAMP,HIGH); // switch on
          digitalWrite(STARTLED,HIGH);   // Pushbutton LED on
          
-         lcd.setCursor ( 0, 1 );        // go to the next line
-         lcd.print ("Action: RUN     ");       
          main_status = RUN;
          break;
       
@@ -246,61 +266,29 @@ void loop()
             
          if(KEYIsPressed(STARTBTN))
          {
+            while(KEYIsPressed(STARTBTN));  /* Wait until the button is released */
+
             digitalWrite(RELAY_LAMP,LOW); // switch off
             digitalWrite(STARTLED,LOW);   // Pushbutton LED off
-            
-            lcd.setCursor ( 0, 1 );        // go to the next line
-            lcd.print ("Action: STOP    ");                   
-
-            while(KEYIsPressed(STARTBTN));  /* Wait until the button is released */
-           
+                      
             main_status = STOP;
          }
-         
-         if(TimerSecCounter == 0)
+         else if(TimerSecCounter == 0)
          {
             digitalWrite(RELAY_LAMP,LOW); // switch off
             digitalWrite(STARTLED,LOW);   // Pushbutton LED off
-
-            lcd.setCursor ( 0, 1 );        // go to the next line
-            lcd.print ("Action: EXPIRED!");       
-            
+          
             main_status = ALR;
          }
          break;
              
       case ALR:
          tone(BUZZER, 550);            /* Emit tone */
-         main_status = STOP;
-         break;
+         main_status = waitingResetTimer(main_status);
+	 break;
       
       case STOP:
-         /* Waiting to press the button */
-         
-         if(toggle_flag)
-         {
-            digitalWrite(STARTLED,LOW);   // Pushbutton LED off
-            toggle_flag=0;
-         }
-         else
-         {
-            digitalWrite(STARTLED,HIGH);   // Pushbutton LED on
-            toggle_flag=1;
-         }
-         delay(200);
-         
-         
-         if(KEYIsPressed(STARTBTN))
-         {
-            while(KEYIsPressed(STARTBTN));  /* Wait until the button is released */
-
-//            while(KEYIsNotPressed(STARTBTN));  /* Wait until the button is pressed */
-//            while(KEYIsPressed(STARTBTN));  /* Wait until the button is released */
-
-            digitalWrite(STARTLED,LOW);   // Pushbutton LED off
-
-            main_status = END;
-         }
+         main_status = waitingResetTimer(main_status);
          break;
          
       case END:      
@@ -319,9 +307,335 @@ void loop()
          displayTime(hour, minute, second);        
          break;
    }
+   
+   displayMenu();
 }
 
 
+
+/**
+ *  @fn displayTime
+ *  @brief Display the time on the first LCD line
+ *  @param hours - value for hours
+ *  @param minutes - value for minutes
+ *  @param seconds - value for seconds
+ */
+void displayTime(short hours, short minutes, short seconds)
+{
+   lcd.setCursor ( 7, 0);        /* Go after the writing Time */
+   lcd.print("     ");
+
+#if defined(DISPLAY_TIME)
+   lcd.setCursor ( 7, 0);        
+   sprintf(buffer, "%02d:%02d:%02d", hours, minutes, seconds);
+   lcd.print(buffer);         
+#else
+   lcd.setCursor ( 10, 1);        
+   lcd.print(TimerSecCounter);     
+#endif
+}
+
+/**
+ *  @fn displayMenu
+ *  @brief Display the menu on the second LCD line
+ *  The function uses a flag to know when to display the title.
+ */
+void displayMenu()
+{
+   short tmp_value = 0;
+   
+   if(main_status == IDLE)
+   {
+      if(force_display_title)
+      {
+         lcd.setCursor ( 0, 1 );        /* Position on the second line */
+         force_display_title = OFF;
+         /* Display menu title */
+         switch(set_status)
+         {
+            case READ_PRESETS:
+            case SAVE_PRESETS:
+               lcd.print (preset_menu);
+               break;
+            case SET_SECONDS:
+            case SET_MINUTES:
+            case SET_HOURS:
+               lcd.print (select_menu);
+	       break;
+         }
+      }   
+
+      /* Display menu value */
+      switch(set_status)
+      {
+         case READ_PRESETS:
+            break;
+         case SET_SECONDS:
+            lcd.setCursor ( 4, 1 );        /* Position on the second line */
+            lcd.print ("Seconds  ");
+            break;
+         case SET_MINUTES:
+            lcd.setCursor ( 4, 1 );        /* Position on the second line */
+            lcd.print ("Minutes  ");
+            break;
+         case SET_HOURS:
+            lcd.setCursor ( 4, 1 );        /* Position on the second line */
+            lcd.print ("Hours    ");
+            break;
+         case SAVE_PRESETS:
+            lcd.setCursor ( 9, 1 );        /* Position on the second line */
+            if(preset_index <= MAX_PRESET)
+            {
+               lcd.print(preset_index);
+            }
+            else
+               lcd.print(" res");
+            break;
+      }
+   }
+   else
+   {
+      if(force_display_title)
+      {
+         lcd.setCursor ( 0, 1 );        /* Position on the second line */
+         lcd.print (run_menu);
+         force_display_title = OFF;
+      }        
+      
+      lcd.setCursor ( 8, 1 );        /* Position on the second line */  
+	   
+      switch(main_status)
+      {
+         case STOP:
+            lcd.print("STOP");
+            break;
+         case ALR:
+            lcd.print("END ");
+            break;
+         default:		 
+            lcd.print("RUN ");
+            break;
+      }
+   }
+}
+
+
+
+/**
+ *  @fn settingManagement
+ *  @brief setting state machine management
+ *  This state machine is working only when the main state machine is in IDLE, otherwise it is never called.
+ */
+void settingManagement()
+{
+   unsigned char enc_read = OFF;
+   unsigned char btn_read = OFF;
+
+   /* Reading selection push button status */
+   if(KEYIsPressed(SELBTN))
+   {
+      while(KEYIsPressed(SELBTN));
+	  btn_read = ON;
+   }
+
+   /* Determine if there is an encoder change value */   
+   if(oldEncoderRead != EncoderRead )
+   {
+      enc_read = ON;	 
+   }
+   
+   /* Continue only if there is an action */
+   if(!enc_read && !btn_read)
+      return;
+	  
+   switch(set_status)
+   {
+      case READ_PRESETS:
+         force_display_title = ON;
+         if(enc_read)
+         {
+            /*
+             *  The preset_index meaning :
+             *  0 - 10 -> preset index - 0 indicates no loading
+             *  11     -> Set time
+             */
+            if(EncoderRead > MAX_PRESET+1)
+               EncoderRead = MAX_PRESET+1;
+            preset_index = EncoderRead;		 
+        }
+		 
+        if(btn_read)
+        {
+	
+           /* Execute action */
+        }
+        break;
+		 
+      case SET_SECONDS:
+         if(enc_read)
+         {
+            if(EncoderRead > 59)
+               EncoderRead = 59;
+            second = EncoderRead;   
+         }
+		 
+         if(btn_read)
+         {
+            /* 
+             *  Button pressed - prepare for the setting of minutes
+             */
+            EncoderRead = minute;
+            oldEncoderRead = minute;
+            set_status = SET_MINUTES;
+         }
+         break;
+
+      case SET_MINUTES:
+         if(enc_read)
+         {
+            if(EncoderRead > 59)
+               EncoderRead = 59;
+            minute = EncoderRead;   
+         }
+
+         if(btn_read)
+         {
+            /* 
+             *  Button pressed - prepare for the setting of hours
+             */
+            EncoderRead = hour;
+            oldEncoderRead = hour;
+	    set_status = SET_HOURS;
+         }
+         break;
+
+      case SET_HOURS:
+         if(enc_read)
+         {
+            if(EncoderRead > 2)
+               EncoderRead = 2;
+            hour = EncoderRead;   
+         }
+		 
+         if(btn_read)
+         {
+            EncoderRead = preset_index;
+            oldEncoderRead = preset_index;
+            set_status = SAVE_PRESETS;
+         }
+         break;
+		 
+      case SAVE_PRESETS:
+         if(enc_read)
+         {
+            /*
+             *  The preset_index meaning :
+             *  0 - 10 -> preset index - 0 indicates no saving and change state
+             *  11     -> reset flash memory
+             */
+            if(EncoderRead > MAX_PRESET+1)
+               EncoderRead = MAX_PRESET+1;
+            preset_index = EncoderRead;		 
+         }
+
+         if(btn_read)
+         {
+            if(preset_index == 0)
+            {
+               /* Do nothing - change state */
+               set_status = READ_PRESETS;
+            }
+            else if(preset_index >= 1 && preset_index <= MAX_PRESET)
+            {
+               /* Save current value in preset indexed - TBD */
+			   
+            }
+            else
+            {
+              		   /* Reset flash memory */
+            }
+	 }
+         break;
+   }
+}
+
+/*-------------------- Utility functions ---------------------------------------*/
+
+/*
+ *  @fn waitingResetTimer
+ *  @brief Wait for the push button pressing. Returns the next state, current one or END
+ */
+unsigned char waitingResetTimer(unsigned char status)
+{
+   /* Waiting to press the button */
+         
+   if(toggle_LED)
+   {
+      digitalWrite(STARTLED,LOW);   // Pushbutton LED off
+      toggle_LED=OFF;
+   }
+   else
+   {
+      digitalWrite(STARTLED,HIGH);   // Pushbutton LED on
+      toggle_LED=ON;
+   }
+   delay(200);
+                
+   if(KEYIsPressed(STARTBTN))
+   {
+      while(KEYIsPressed(STARTBTN));  /* Wait until the button is released */
+
+      digitalWrite(STARTLED,LOW);   // Pushbutton LED off
+      return(END);
+   }
+   return(status);
+}
+
+
+/**
+ *  @fn secondsToTime
+ *  @brief The function has the purpose to convert a single value in seconds into
+ *  the more traditional hours/minutes/seconds display
+ *  @param raw_seconds - number of seconds to convert
+ *  @param hours - pointer to the location where to store the hour value
+ *  @param minutes - pointer to the location where to store the minutes value
+ *  @param seconds - pointer to the location where to store the seconds value
+ *  No return value
+ */
+void secondsToTime(int raw_seconds, short *hours, short *minutes, short *seconds )
+{
+   int temp=0;
+
+   *hours = raw_seconds/3600;
+   temp = *hours * 3600;
+   *minutes = (raw_seconds - temp)/60;
+   temp += *minutes * 60;
+   *seconds = raw_seconds - temp;
+}
+
+/*
+ *  @fn timeToSeconds
+ *  @brief The function has the purpose to create a single value in seconds from hour/minutes/seconds
+ *  @param hours - value of hours, from 0 to 2
+ *  @param minutes - value of minutes, from 0 to 59
+ *  @param seconds - value of seconds, from 0 to 59
+ *  Return the number of seconds
+ */
+int timeToSeconds(int hours, int minutes, int seconds )
+{
+   int raw_seconds;
+   
+   raw_seconds = (hours * 3600) + (minutes * 60) + seconds;
+   
+   return(raw_seconds);
+}
+
+
+
+/*
+ * ----------- OLD functions ----- to be deleted -------------------------------- 
+ */
+#if 0 
 /*
  *  settingManagement function
  *  This function manage the setting state machine.
@@ -371,7 +685,7 @@ void settingManagement()
 
        set_status++;
        if(set_status > SAVE_PRESETS)
-          set_status = LOAD_PRESETS;
+          set_status = READ_PRESETS;
 	  
    }
 }
@@ -416,7 +730,7 @@ void displaySetting()
             {
 	       case 0:
                   lcd.print ("Save ?      No  ");
-                  break;
+                  break;+++
 	      case 1:
                   lcd.print ("Save ?      Yes ");
 		  break;		  
@@ -435,213 +749,6 @@ void displaySetting()
    }
 }
 
-/**
- *  @fn displayTime
- *  @brief Display the time on the first LCD line
- *  @param hours - value for hours
- *  @param minutes - value for minutes
- *  @param seconds - value for seconds
- */
-void displayTime(short hours, short minutes, short seconds)
-{
-   lcd.setCursor ( 7, 0);        /* Go after the writing Time */
-   lcd.print("     ");
 
-#if DISPLAY_TIME
-   lcd.setCursor ( 7, 0);        
-   sprintf(buffer, "%02d:%02d:%02d", hours, minutes, seconds);
-   lcd.print(buffer);         
-#else
-   lcd.setCursor ( 10, 1);        
-   lcd.print(TimerSecCounter);     
 #endif
-}
-
-/**
- *  @fn displayMenu
- *  @brief Display the menu on the second LCD line
- *  @param hours - value for hours
- *  @param minutes - value for minutes
- *  @param seconds - value for seconds
- */
-void displayMenu(short hours, short minutes, short seconds)
-{
-   short tmp_value = 0;
-
-   
-   lcd.setCursor ( 0, 1 );        /* Position on the second line */
-
-   /* Display menu title */
-   switch(set_status)
-   {
-      case LOAD_PRESETS:
-	     break;
-      case SET_SECONDS:
-      case SET_MINUTES:
-      case SET_HOURS:
-         lcd.print (set_menu);
-	     break;
-      case SAVE_PRESETS:
-	     break;
-   }
-
-   /* Display menu value */
-   switch(set_status)
-   {
-      case LOAD_PRESETS:
-	     break;
-      case SET_SECONDS:
-         lcd.setCursor ( 4, 1 );        /* Position on the second line */
-		 
-      case SET_MINUTES:
-         lcd.setCursor ( 4, 1 );        /* Position on the second line */
-      case SET_HOURS:
-         lcd.setCursor ( 4, 1 );        /* Position on the second line */
-         lcd.print (set_menu);
-	     break;
-      case SAVE_PRESETS:
-	     break;
-   }
-}
-
-
-
-
-
-/**
- *  @fn secondsToTime
- *  @brief The function has the purpose to convert a single value in seconds into
- *  the more traditional hours/minutes/seconds display
- */
-void secondsToTime(int raw_seconds, short *hours, short *minutes, short *seconds )
-{
-   int temp=0;
-
-   *hours = raw_seconds/3600;
-   temp = *hours * 3600;
-   *minutes = (raw_seconds - temp)/60;
-   temp += *minutes * 60;
-   *seconds = raw_seconds - temp;
-}
-
-/*
- *  timeToSeconds
- *  The function has the purpose to create a single value in seconds from hour/minutes/seconds
- */
-int timeToSeconds(int hours, int minutes, int seconds )
-{
-   int raw_seconds;
-   
-   raw_seconds = (hours * 3600) + (minutes * 60) + seconds;
-   
-   return(raw_seconds);
-}
-
-
-
-/**
- *  @fn settingManagement
- *  @brief setting state machine management
- *  This state machine is working only when the main state machine is in IDLE, otherwise it is never called.
- */
-void settingManagement()
-{
-   unsigned char enc_read = OFF;
-   unsigned char btn_read = OFF;
-
-   /* Reading selection push button status */
-   if(KEYIsPressed(SELBTN))
-   {
-      while(KEYIsPressed(SELBTN));
-	  btn_read = ON;
-   }
-
-   /* Determine if there is an encoder change value */   
-   if(oldEncoderRead != EncoderRead )
-   {
-      enc_read = ON;	 
-   }
-   
-   /* Continue only if there is an action */
-   if(!enc_read && !btn_read)
-      return;
-	  
-   switch(set_status)
-   {
-      case LOAD_PRESETS:
-         if(enc_read)
-         {
-		 
-		 }
-		 
-		 if(btn_read)
-		 {
-		    /* Execute action */
-		 }
-		 break;
-		 
-      case SET_SECONDS:
-         if(enc_read)
-         {
-            if(EncoderRead > 59)
-               EncoderRead = 59;
-            second = EncoderRead;   
-         }
-		 
-		 if(btn_read)
-		 {
-            EncoderRead = minute;
-            oldEncoderRead = minute;
-		    set_status = SET_MINUTES;
-		 }
-         break;
-
-      case SET_MINUTES:
-         if(enc_read)
-         {
-            if(EncoderRead > 59)
-               EncoderRead = 59;
-            minute = EncoderRead;   
-         }
-
-		 if(btn_read)
-		 {
-            EncoderRead = hour;
-            oldEncoderRead = hour;
-		    set_status = SET_HOURS;
-		 }
-         break;
-
-      case SET_HOURS:
-         if(enc_read)
-         {
-            if(EncoderRead > 2)
-               EncoderRead = 2;
-            hour = EncoderRead;   
-         }
-		 
-		 if(btn_read)
-		 {
-            EncoderRead = minute;
-            oldEncoderRead = minute;
-		    set_status = SAVE_PRESETS;
-		 }
-         break;
-		 
-      case SAVE_PRESETS:
-
-         if(btn_read)
-		 {
-		    set_status = LOAD_PRESETS;
-		 }
-	     break;
-   }
-}
-
-
-
-
-
-
-
 
